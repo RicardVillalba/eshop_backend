d54d699de8aaab063a86ca52fbdaff5f39332e3c
RicardVillalba <ricard@MacBook-Pro-de-Ricard.local>
2021-03-14 18:23:40 +0100

    categories routers created

--- routes/categories.js
+++ routes/categories.js
@@ -3,24 +3,68 @@ const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const CategoriesList = await Category.find();
-  if (!CategoriesList) {
+  const categoriesList = await Category.find();
+  if (!categoriesList) {
     res.status(500).json({ success: false });
   }
-  res.send(CategoriesList);
+  res.status(200).send(categoriesList);
+});
+
+router.get("/:id", async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    res.status(500).json({ message: "category whith the given id not found" });
+  }
+  res.status(200).send(category);
 });
 
 router.post("/", async (req, res) => {
   let category = new Category({
     name: req.body.name,
     icon: req.body.icon,
-    color: req.body.icon,
+    color: req.body.color,
   });
   category = await category.save();
   if (!category) {
-    res.status(404).send("category cannot be created");
+    res.status(400).send("category cannot be created");
   }
-  res.send(category);
+  res.status(200).send(category);
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!category) {
+    res.status(400).send("category cannot be updated");
+  }
+  res.status(200).send(category);
+});
+
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res
+          .status(200)
+          .json({ success: true, message: "the category is removed" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "category not found" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
 });
 
 module.exports = router;
